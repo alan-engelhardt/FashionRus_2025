09e97bf651e300a09040b70ec7cc4a8a506d688a
Alan <ale@kea.dk>
2025-01-30 17:54:33 +0100

    add dynamic image id and name

--- js/produktliste.js
+++ js/produktliste.js
@@ -1,8 +1,28 @@
-const productContainer = document.querySelector("main");
+const listContainer = document.querySelector("main");
 fetch(`https://kea-alt-del.dk/t7/api/products`)
   .then((response) => response.json())
   .then(showProductLst);
 
 function showProductLst(data) {
   console.log(data);
+  let markup = "<h2>Apparel</h2>";
+  data
+    .map(
+      (product) =>
+        (markup += `
+    <article class="smallProduct">
+      <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="product image" />
+      <h3>${product.productdisplayname}</h3>
+      <p class="subtle">Tshirts | Nike</p>
+      <p class="price">DKK <span>1595</span>,-</p>
+      <div class="discounted">
+        <p>Now DKK <span></span>,-</p>
+        <p><span></span>%</p>
+      </div>
+      <a href="product.html">Read More</a>
+    </article>
+  `)
+    )
+    .join("");
+  listContainer.innerHTML = markup;
 }
