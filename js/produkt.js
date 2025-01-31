5f8b97ec64e8f69b643ebfb49f5c28fe8cffccb6
Alan <ale@kea.dk>
2025-01-31 09:44:12 +0100

    test

--- js/produkt.js
+++ js/produkt.js
@@ -1,27 +1,28 @@
-const productContainer = document.querySelector("#productContainer");
 const productId = 1530;
+const productContainer = document.querySelector("#productContainer");
+
 fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
   .then((response) => response.json())
-  .then((data) => showProduct(data));
+  .then(showProduct);
 
 function showProduct(data) {
   productContainer.innerHTML = `
-    <figure>
-        <img
-          src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp"
-          alt="Produktbillede"
-          class="productImage"
-        />
-        <span class="saleLabel">Udsalg!</span>
-      </figure>
-      <section class="productDetails">
-        <h2 class="productName">${data.productdisplayname}</h2>
-        <div>
-        <p class="articleType"><span class="bold">Type:</span> ${data.articletype}</p>
-          <p class="productCategory"><span class="bold">Kategori:</span> ${data.category}</p>
-          <p class="productPrice"><span class="bold">Pris:</span> ${data.price},-</p>
-        </div>
-        <button class="buyButton">Køb nu</button>
-      </section>
-    `;
+      <figure>
+          <img
+            src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp"
+            alt="Produktbillede"
+            class="productImage"
+          />
+          <span class="saleLabel">Udsalg!</span>
+        </figure>
+        <section class="productDetails">
+          <h2 class="productName">${data.productdisplayname}</h2>
+          <div>
+          <p class="articleType"><span class="bold">Type:</span> ${data.articletype}</p>
+            <p class="productCategory"><span class="bold">Kategori:</span> ${data.category}</p>
+            <p class="productPrice"><span class="bold">Pris:</span> ${data.price},-</p>
+          </div>
+          <button class="buyButton">Køb nu</button>
+        </section>
+      `;
 }
