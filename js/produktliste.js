f851508acb5db00c56852bf9de2d76158b582937
Alan <ale@kea.dk>
2025-02-01 19:24:38 +0100

    show discount

--- js/produktliste.js
+++ js/produktliste.js
@@ -7,14 +7,14 @@ fetch(`https://kea-alt-del.dk/t7/api/products?category=${category}`)
   .then((products) => {
     const markup = products
       .map(
-        (product) => `<article class="smallProduct ${product.soldout ? "soldOut" : ""}">
+        (product) => `<article class="smallProduct ${product.soldout && "soldOut"} ${product.discount && "onSale"}">
           <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="product image" />
           <h3>${product.productdisplayname}</h3>
           <p class="subtle">Tshirts | Nike</p>
           <p class="price">DKK <span>${product.price}</span>,-</p>
           <div class="discounted">
-            <p>Now DKK <span></span>,-</p>
-            <p><span></span>%</p>
+            <p>Now DKK <span>${Math.round(product.price - (product.price * product.discount) / 100)}</span>,-</p>
+            <p><span>${product.discount}</span>%</p>
           </div>
           <a href="product.html?id=${product.id}">Read More</a>
         </article>`
