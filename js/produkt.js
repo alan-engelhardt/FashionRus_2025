bf0c01d03f921f9b8471b3b6a26ebb2350b7e856
alan-engelhardt <ale@ek.dk>
2026-02-23 10:26:54 +0100

    formater

--- js/produkt.js
+++ js/produkt.js
@@ -8,24 +8,24 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
 function renderProduct(data) {
   productContainer.innerHTML = `
   <div>
-  <a href="productlist.html?category=${data.category}">Tilbage</a>
-      <figure>
-          <img
-            src="https://kea-alt-del.dk/t7/images/webp/640/${data.id}.webp"
-            alt="Produktbillede"
-            class="productImage"
-          />
-          ${data.discount ? `<span class="saleLabel">Udsalg!</span>` : ""}
-        </figure>
-        </div>
-        <section class="productDetails">
-          <h2 class="productName">${data.productdisplayname}</h2>
-          <div>
-          <p class="articleType"><span class="bold">Type:</span> ${data.articletype}</p>
-            <p class="productCategory"><span class="bold">Kategori:</span> ${data.category}</p>
-            <p class="productPrice"><span class="bold">Pris:</span> ${data.price},-</p>
-          </div>
-          <button class="buyButton">Køb nu</button>
-        </section>
-      `;
+    <a href="productlist.html?category=${data.category}">Tilbage</a>
+    <figure>
+      <img
+        src="https://kea-alt-del.dk/t7/images/webp/640/${data.id}.webp"
+        alt="Produktbillede"
+        class="productImage"
+      />
+      ${data.discount ? `<span class="saleLabel">Udsalg!</span>` : ""}
+    </figure>
+  </div>
+  <section class="productDetails">
+    <h2 class="productName">${data.productdisplayname}</h2>
+      <div>
+        <p class="articleType"><span class="bold">Type:</span> ${data.articletype}</p>
+        <p class="productCategory"><span class="bold">Kategori:</span> ${data.category}</p>
+        <p class="productPrice"><span class="bold">Pris:</span> ${data.price},-</p>
+      </div>
+    <button class="buyButton">Køb nu</button>
+  </section>
+  `;
 }
